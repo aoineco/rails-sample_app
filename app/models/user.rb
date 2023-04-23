88d975c3fbe1de85392cbfdd8d93cc701159d49a
aoineco <masaya891047@gmail.com>
2023-04-23 02:42:15 +0000

    Add functionality for user login persistence

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,11 @@ class User < ApplicationRecord
     remember_digest
   end
 
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
   # セッションハイジャック防止のためにセッショントークンを返す
   # この記憶ダイジェストを再利用しているのは単に利便性のため
   def session_token
