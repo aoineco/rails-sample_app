dedeacb70b57f09c6176ec2e045931cd348dd8b8
aoineco <masaya891047@gmail.com>
2023-04-29 06:34:48 +0000

    Add forgetting user feature and related tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ApplicationRecord
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
@@ -39,6 +40,7 @@ class User < ApplicationRecord
     remember_digest || remember
   end
 
+  # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
